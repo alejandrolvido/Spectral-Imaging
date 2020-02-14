31a11bc5d73e4c3c016bcc638635b2de74d80bb9
alejandrolvido <alejandrolvido@gmail.com>
2020-02-13 21:37:36 -0600

    Add files via upload

---
+++ NeonGreen_signal_example/OAM2002_Bleedthru_test_autocorr_1.m
@@ -0,0 +1,27 @@
+
+% tim interval
+initial_time=1;
+final_time=20;
+
+% image bondaries
+A1(1,1)=210;%up
+A1(1,2)=240;%down
+A1(2,1)=290;%right
+A1(2,2)=340;%left
+
+base_name1=[cd '\img_'];
+
+%chanel to test
+C_615='615 nm_000';
+C_CyOFP1='470 GFP_000';
+C_BF='BrightField_000';
+C_NG='505 mNG_000';
+C_Ru='555 mRuby3_000';
+C_mKOk='555 new_mKok_000';
+C_mTFP1='470 mTFP_000';
+type='.tif';
+Channel=C_NG;
+
+[bld1,bld2,bld3,bld4,bld5] = Auto_liBox(initial_time,A1,final_time,base_name1,Channel,type);
+
+
