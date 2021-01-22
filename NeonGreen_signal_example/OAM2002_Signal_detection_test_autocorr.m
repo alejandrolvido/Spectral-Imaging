d0ae90a3b63c52224d547e9c049bf4aee4fbe290
alejandrolvido <alejandrolvido@gmail.com>
2021-01-22 12:21:31 -0600

    Update OAM2002_Signal_detection_test_autocorr.m

--- NeonGreen_signal_example/OAM2002_Signal_detection_test_autocorr.m
+++ NeonGreen_signal_example/OAM2002_Signal_detection_test_autocorr.m
@@ -1,12 +1,13 @@
-%% this is an example of how to use the function Auto_liBox after deciding wich region of an image to analyse
+%% this is an example of how to use the function Auto_liBox after deciding the ROI (region of interest) to be analysed
+%% the ROI here is defined manually for the exmaple, but is define by the segementing algorithm in the pipeline. 
 
 clearvars
 
-% tim interval
+% time series interval  
 initial_time=1;
 final_time=20;
 
-% image bondaries
+% manually selected image bondaries around the cell OR defined the segmenting algorithm 
 A1(1,1)=210;%up
 A1(1,2)=240;%down
 A1(2,1)=290;%right
@@ -14,7 +15,7 @@ A1(2,2)=340;%left
 
 base_name1=[cd '\img_'];
 
-%chanel to test
+%channel to test
 C_615='615 nm_000';
 C_CyOFP1='470 GFP_000';
 C_BF='BrightField_000';
@@ -25,6 +26,7 @@ C_mTFP1='470 mTFP_000';
 type='.tif';
 Channel=C_NG;
 
+% out put of this function are versions of the original image, assigning probabilities to each pixel of containing an image 
 [bld1,bld2,bld3,bld4,bld5] = Auto_ljBox(initial_time,A1,final_time,base_name1,Channel,type);
 
 
